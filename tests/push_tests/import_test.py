26938b6adc22754d9d4d53470a7178c389a6769c
Kobe <kobe.ande@gmail.com>
2024-03-20 12:00:46 +0100

    import test

--- tests/push_tests/import_test.py
+++ tests/push_tests/import_test.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 lib_folder = Path(__file__).resolve().parents[2].joinpath("src")
 
+sys.path.insert(0, str(lib_folder))
 import valenspy
 
 print(f"Successfully imported valenspy version: {valenspy.__version__}")
