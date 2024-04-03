eff18e17629f42f19fd00a84b0279b4d722a8add
kobebryant432 <kobe.ande@gmail.com>
2024-04-03 14:21:35 +0200

    Input converter

---
+++ src/valenspy/inputconverter_functions.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+def _non_convertor(file: Path) -> Path:
+    """A dummy function that does not convert the input file as it already is in the correct format."""
+    return file
+
