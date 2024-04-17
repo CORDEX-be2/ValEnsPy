f9d49cf76232cd9dc3d535d8b4ddfecfb5a69a9a
kobebryant432 <kobe.ande@gmail.com>
2024-04-17 09:07:39 +0200

    cf checking example

--- src/valenspy/__init__.py
+++ src/valenspy/__init__.py
@@ -9,6 +9,20 @@ from valenspy.inputconverter import InputConverter
 from valenspy.preprocessor import Preprocessor
 from valenspy.diagnostic import Diagnostic, Model2Ref, Ensemble2Ref
 import valenspy.preprocessing_tasks
+import valenspy.cf_checks
+
+# =============================================================================
+# Demo-datafile
+# =============================================================================
+
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_PATH)
+
+# demo files
+
+demo_data_CF = os.path.join(
+    BASE_PATH, "valenspy", "datafiles", "tas_Amon_EC-Earth3-Veg_historical_r1i1p1f1_gr_195301-195312.nc"
+)
 
 # =============================================================================
 # Pre-made diagnostics
