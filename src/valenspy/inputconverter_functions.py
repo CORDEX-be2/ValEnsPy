90f62914214a18875d5d3909a66313b08a8e3d7e
Inne Vanderkelen <inne.vanderkelen@kuleuven.be>
2024-05-21 14:32:37 +0200

    Add EOBS input convertor - initial attempt

--- src/valenspy/inputconverter_functions.py
+++ src/valenspy/inputconverter_functions.py
@@ -7,10 +7,37 @@ def _non_convertor(file: Path) -> Path:
     return file
 
 
-def EOBS_to_CF(file: Path) -> Path:
-    """Convert the EOBS netCDF file to a netCDF file in CF convention."""
-    # Open the EOBS file
-    ds = xr.open_dataset(file)
+# work in progress. 
+def EOBS_to_CF(directory: Path, variable: string) -> Path:
+    """Convert the EOBS netCDF file to a netCDF file in CF convention for specified CF variable."""
 
-    # Rename the dimensions
-    ds = ds.rename_dims({"latitude": "lat", "longitude": "lon"})
+    # load look-up table of EOBS variables to CORDEX variables
+    df_lookup_EOBS = pd.read_csv('../ancilliary_data/lookuptable_EOBS.csv', sep=";")
+
+    # get E-OBS variable corresponding to the requested variable using its look-up table
+    EOBS_variable = df_lookup_EOBS.loc[df_lookup_EOBS['variable_name'] == variable,'EOBS_name'].values[0]
+
+    # open the EOBS file for the corresponding variable
+    files = list(directory.glob("*"+EOBS_variable+"*mean*.nc")) #Select all the netCDF files in the directory
+
+    ds = xr.open_mfdataset(files, combine='by_coords', chunks='auto')
+
+    # make EOBS CF compliant (and CMORized)
+    ds = ds.rename_vars({EOBS_variable: variable})
+    ds = ds.rename({"latitude": "lat", "longitude": "lon"})
+
+    # Convert from Celsius to Kelvin -- to do: put this in a seperate convertor to be used by other functions? 
+    if variable == 'tas': 
+        ds[variable] = ds[variable] + 273.15
+
+    # add necessary metadata -- this makes the person including the obs data think whether additional processing is needed 
+
+    ds.attrs["long_name"] = df_cf_lookup.loc[df_cf_lookup['variable_name'] == variable,'long_name'].values[0] # from the CORDEX look-up table
+    ds.attrs["original_name"] = EOBS_variable
+    ds.attrs["original_long_name"] = df_lookup_EOBS.loc[df_lookup_EOBS['variable_name'] == variable,'EOBS_long_name'].values[0]
+    ds.attrs["units"] = df_cf_lookup.loc[df_cf_lookup['variable_name'] == variable,'units'].values[0] # from the CORDEX look-up table 
+    ds.attrs["time_freq"] = "daily"  # possible values: daily, hourly, monthly, yearly
+    ds.attrs["spatial_resolution"] = "0.1deg"  
+    ds.attrs["domain"] = "europe"
+
+    return ds
