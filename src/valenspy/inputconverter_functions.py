ad399db1452d26ad5c550d2bd821e27d4648a6cb
Inne Vanderkelen <inne.vanderkelen@kuleuven.be>
2024-05-24 12:31:46 +0200

    add helper functions for unit conversion

--- src/valenspy/inputconverter_functions.py
+++ src/valenspy/inputconverter_functions.py
@@ -58,11 +58,11 @@ def EOBS_to_CF(file: Path) -> Path:
 
         # Unit conversion - hard coded EOBS units for units different to CORDEX
         if obs_LOOKUP[var]['obs_units'] == 'Celcius': 
-            ds[var] = ds[var] + 273.15 # Celcius to Kelvin
-        elif obs_LOOKPUP[var]['obs_units'] == 'hPa': 
-            ds[var] = ds[var] * 100 # hPa to Pa
-        elif obs_LOOKPUP[var]['obs_units'] == 'mm': # ! note observations remain daily time frequency
-            ds[var] = ds[var] / 86400 # mm to kg m^-2 s^-1 
+            ds[var] = _convert_Celcius_to_Kelvin(ds[var]) 
+        elif obs_LOOKUP[var]['obs_units'] == 'hPa': 
+            ds[var] = _convert_hPa_to_Pa(ds[var]) # hPa to Pa
+        elif obs_LOOKUP[var]['obs_units'] == 'mm': # ! note observations remain daily time frequency
+            ds[var] = _convert_mm_to_kg_m2_s1(ds[var]) # mm to kg m^-2 s^-1 
 
         # update unit attribute
         ds[var].attrs["units"] = CORDEX_VARIABLES[var]["units"] # from the CORDEX look-up table 
@@ -76,8 +76,12 @@ def EOBS_to_CF(file: Path) -> Path:
         # rename dimensions
         ds[var] = ds[var].rename({"latitude": "lat", "longitude": "lon"})
 
+        # convert the time dimension to a pandas datetime index --  do we want this to happen within the convertor? Or do we leave it up to the user? 
+        ds[var]['time'] = pd.to_datetime(ds[var].time)
+
+
         # additional attributes -- hard coded for EOBS
-        ds[var].attrs["time_freq"]          = "daily"  # possible values: daily, hourly, monthly, yearly
+        ds[var].attrs["freq"]               = "daily"  # possible values: daily, hourly, monthly, yearly
         ds[var].attrs["spatial_resolution"] = "0.1deg"  
         ds[var].attrs["domain"]             = "europe"
 
@@ -89,4 +93,135 @@ def EOBS_to_CF(file: Path) -> Path:
 
 
 
+# helper functions for unit conversion - can be moved to more appropriate place 
 
+## imports for helpen functions
+import xarray as xr
+import pandas as pd
+import numpy as np
+
+# Do we want other possible inputs than data arrays? 
+def _convert_Celcius_to_Kelvin(da: xr.DataArray): 
+
+    """
+    Convert values in xarray DataArray from °C to K
+
+    Parameters
+    ----------
+    da : xr.DataArray
+        The xarray DataArray to convert
+
+    Returns
+    -------
+    xr.DataArray
+        The  converted xarray DataArray
+    """
+    
+    # do conversion
+    da = da + 273.15 # Celcius to Kelvin
+
+    # update units attribute --  naming of units defined in ./src/valenspy/ancilliary_data/CORDEX_variables.yml
+    da["units"] = "K" 
+
+    return da
+
+def _convert_Kelvin_to_Celcius(da: xr.DataArray): 
+
+    """
+    Convert values in xarray DataArray from K to °C
+
+    Parameters
+    ----------
+    da : xr.DataArray
+        The xarray DataArray to convert
+
+    Returns
+    -------
+    xr.DataArray
+        The  converted xarray DataArray
+    """
+    
+    # do conversion
+    da = da - 273.15 # Kelvin to Celcius
+
+    # update units attribute
+    da["units"] = "°C" 
+
+    return da
+
+def _convert_hPa_to_Pa(da: xr.DataArray): 
+
+    """
+    Convert values in xarray DataArray from hPa to Pa
+
+    Parameters
+    ----------
+    da : xr.DataArray
+        The xarray DataArray to convert
+
+    Returns
+    -------
+    xr.DataArray
+        The  converted xarray DataArray
+    """
+    
+    # do conversion
+    da = da * 100 
+
+    # update units attribute
+    da["units"] = "Pa" 
+
+    return da
+
+def _convert_Pa_to_hPa(da: xr.DataArray): 
+
+    """
+    Convert values in xarray DataArray from Pa to hPa
+
+    Parameters
+    ----------
+    da : xr.DataArray
+        The xarray DataArray to convert
+
+    Returns
+    -------
+    xr.DataArray
+        The  converted xarray DataArray
+    """
+    
+    # do conversion
+    da = da / 100 
+
+    # update units attribute
+    da["units"] = "hPa" 
+
+    return da
+
+
+# better function name welcome! 
+def _convert_mm_to_kg_m2_s1(da: xr.DataArray): 
+
+    """
+    Convert daily (!) values in xarray DataArray from mm to kg m^-2 s^-1
+
+    Parameters
+    ----------
+    da : xr.DataArray
+        The xarray DataArray to convert
+
+    Returns
+    -------
+    xr.DataArray
+        The  converted xarray DataArray
+    """
+    
+    # first, get timestep (frequency) by calculating the difference between the first consecutive time values in seconds
+    timestep_nseconds = da.time.diff(dim='time').values[0]/  np.timedelta64(1, 's')
+
+    # do conversion
+    da = da / timestep_nseconds # mm to kg m^-2 s^-1  
+
+    # update units attribute
+    da["units"] = "kg m-2 s-1" 
+
+    return da
