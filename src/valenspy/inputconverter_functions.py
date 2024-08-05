0bbc83a4a018eea130b00cea93af03cb39c91c11
Inne Vanderkelen <inne.vanderkelen@kuleuven.be>
2024-08-05 10:35:00 +0200

    make sure ERA5 coordinates are sorted ascending - important for region selection

--- src/valenspy/inputconverter_functions.py
+++ src/valenspy/inputconverter_functions.py
@@ -196,6 +196,9 @@ def ERA5_to_CF(ds: xr.Dataset, metadata_info=None) -> Path:
             if "lat" not in ds[var].coords:
                 ds = ds.rename({"latitude": "lat"})
 
+            # make sure lat and lon are sorted ascending
+            ds = ds.sortby('lat').sortby('lon')
+            
             # bugfix ERA5 (found in clh): replace valid_time by time
             if "time" not in ds: 
                 ds = ds.rename({'valid_time':'time'})
