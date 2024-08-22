209216a841f2020966209ab3728d3dee508f8286
kobebryant432 <kobe.ande@gmail.com>
2024-08-22 12:10:43 +0200

    improved test

--- tests/push_tests/test_basic_unit_conversion_logic.py
+++ tests/push_tests/test_basic_unit_conversion_logic.py
@@ -4,24 +4,21 @@ from valenspy._unit_conversions import unit_conversion_functions, EQUIVALENT_UNI
 import pytest
 
 @pytest.mark.parametrize("lookup_name", ["EOBS_lookup", "CCLM_lookup", "ALARO-SFX_K_lookup","ERA5_lookup"])
-def test_lookup_unit_coverage(lookup_name):
+def test_lookup_unit_conversion_coverage(lookup_name):
     """
-    Testing whether the units defined in a lookup table are either:
-    1. Already in one of the CF defined conversion units
-    2. Will be converted to one of the CF defined conversion units as it is defined in
-        a. The unit_conversion_functions - a dictionary linking raw_units to the conversion function to the desired units
-        b. The EQUIVALENT_UNITS - a dictionary listing all equivalent units which are linked to either a unit which can be converted to a CF unit or a CF unit itself
+    Test to see if there are any units in the lookup table which should be converted but are not defined in the unit_conversion_functions or EQUIVALENT_UNITS.
     """
     CORDEX_VARIABLES = load_yml("CORDEX_variables")
-
     cf_units = set([var_attr["units"] for _, var_attr in CORDEX_VARIABLES.items()])
+
     equivalent_units = set([unit for unit in EQUIVALENT_UNITS])
     convertable_units = set([unit for unit in unit_conversion_functions])
-    covered_units = cf_units.union(equivalent_units).union(convertable_units)
+    all_convertable_units = equivalent_units.union(convertable_units)
 
     lookup_table = load_yml(lookup_name)
-    raw_units = set([var_attr["raw_units"] for  _, var_attr in lookup_table.items()])
-    assert raw_units.issubset(covered_units), f"The following units in {lookup_name}.yml are not covered: {raw_units - covered_units}"
+    to_convert_raw_units = set([var_attr["raw_units"] for  var, var_attr in lookup_table.items() if var_attr["raw_units"] != CORDEX_VARIABLES[var]["units"]])
+    
+    assert to_convert_raw_units.issubset(all_convertable_units), f"The following units in {lookup_name}.yml should be converted for some variable but are not: {to_convert_raw_units - all_convertable_units}"
 
 def test_overlap_between_CORDEX_units_and_convertable_units():
     """
@@ -36,3 +33,4 @@ def test_overlap_between_CORDEX_units_and_convertable_units():
     cf_units = set([var_attr["units"] for _, var_attr in CORDEX_VARIABLES.items()])
     
     assert cf_units.isdisjoint(all_convertable_units), f"The following units are both in the CORDEX variables and the equivalent_units or unit_conversion_functions: {cf_units.intersection(all_convertable_units)}"
+
