630d1d5ce9d765d58096c0e3590211ace5f234e7
kobebryant432 <kobe.ande@gmail.com>
2024-08-30 12:12:25 +0200

    Updated __init__ for different access

--- src/valenspy/__init__.py
+++ src/valenspy/__init__.py
@@ -1,10 +1,6 @@
 import os
 import sys
 from pathlib import Path
-
-BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_PATH)
-
 import valenspy.input.converter_functions
 from valenspy.input import InputConverter, INPUT_CONVERTORS
 import valenspy.processing
@@ -12,22 +8,6 @@ from valenspy.diagnostic import Diagnostic, Model2Ref, Ensemble2Ref, Ensemble2Se
 from valenspy.input import InputManager
 
 from valenspy._utilities import is_cf_compliant, cf_status
-
-# =============================================================================
-# Demo-datafile
-# =============================================================================
-
-BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_PATH)
-
-# demo files
-
-demo_data_CF = os.path.join(
-    BASE_PATH,
-    "valenspy",
-    "datafiles",
-    "tas_Amon_EC-Earth3-Veg_historical_r1i1p1f1_gr_195301-195312.nc",
-)
 
 # =============================================================================
 # Version
