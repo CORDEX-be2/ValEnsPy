b42d146d67f4399fb06dba85c8fdf1c743a60b1f
kobebryant432 <kobe.ande@gmail.com>
2024-09-11 09:11:44 +0200

    update diagnostic init

--- src/valenspy/diagnostic/__init__.py
+++ src/valenspy/diagnostic/__init__.py
@@ -1,8 +1,8 @@
 from .diagnostic import Diagnostic, Model2Ref, Ensemble2Ref, Ensemble2Self
 from .diagnostic import (
-    DiurnalCycle
-    TimeSeriesSpatialMean
-    SpatialBias
-    TemporalBias
-    DiurnalCycleBias
+    DiurnalCycle,
+    TimeSeriesSpatialMean,
+    SpatialBias,
+    TemporalBias,
+    DiurnalCycleBias,
 )
