f15412a5d1ef3dff583cd9ec302ec6aae5209408
kobebryant432 <kobe.ande@gmail.com>
2024-09-12 16:55:00 +0200

    test clean up

--- tests/push_tests/test_basic_unit_conversion_logic.py
+++ tests/push_tests/test_basic_unit_conversion_logic.py
@@ -3,10 +3,14 @@ from valenspy._unit_conversions import UNIT_CONVERSION_FUNCTIONS, EQUIVALENT_UNI
 
 import pytest
 
-@pytest.mark.parametrize("lookup_name", ["EOBS_lookup", "CCLM_lookup", "ALARO-SFX_K_lookup","ERA5_lookup","CLIMATE_GRID_lookup"])
+lookup_tables = ["EOBS_lookup", "CCLM_lookup", "ALARO-SFX_K_lookup","ERA5_lookup","CLIMATE_GRID_lookup", "RADCLIM_lookup"]
+
+@pytest.mark.parametrize("lookup_name", lookup_tables)
 def test_lookup_unit_conversion_coverage(lookup_name):
     """
     Test to see if there are any units in the lookup table which should be converted but are not defined in the UNIT_CONVERSION_FUNCTIONS or EQUIVALENT_UNITS.
+    The UNIT_CONVERSION_FUNCTIONS contain the mapping of units to the conversion functions - indicating that these units can be converted.
+    The EQUIVALENT_UNITS contain the mapping of units that are equivalent to each other - indicating that these units can be converted to equivalent units which are defined in the UNIT_CONVERSION_FUNCTIONS.
     """
     CORDEX_VARIABLES = load_yml("CORDEX_variables")
     cf_units = set([var_attr["units"] for _, var_attr in CORDEX_VARIABLES.items()])
@@ -19,6 +23,19 @@ def test_lookup_unit_conversion_coverage(lookup_name):
     to_convert_raw_units = set([var_attr["raw_units"] for  var, var_attr in lookup_table.items() if var_attr["raw_units"] != CORDEX_VARIABLES[var]["units"]])
     
     assert to_convert_raw_units.issubset(all_convertable_units), f"{to_convert_raw_units - all_convertable_units} in {lookup_name}.yml should be converted for some variable but are not."
+
+@pytest.mark.parametrize("lookup_name", lookup_tables)
+def test_lookup_raw_name_uniqueness(lookup_name):
+    """
+    Test whether the raw names in each lookup tables are unique, either as a single string or a unique combination of strings and aggregation functions.
+    """
+    lookup_table = load_yml(lookup_name)
+
+    raw_names = [var_attr["raw_name"] for var_attr in lookup_table.values()]
+    unique_raw_names = set(raw_names)
+    non_unique_raw_names = set([raw_name for raw_name in raw_names if raw_names.count(raw_name) > 1])
+
+    assert len(non_unique_raw_names) == 0, f"Raw names {non_unique_raw_names} in {lookup_name}.yml are not unique."
 
 def test_equivalent_units_definition():
     """
