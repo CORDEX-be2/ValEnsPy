14aa9055e5677e23adf56056e1e3d51bc5ee249a
kobebryant432 <kobe.ande@gmail.com>
2024-10-29 11:38:02 +0100

    import all processing (test)

--- src/valenspy/__init__.py
+++ src/valenspy/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from valenspy.input import InputConverter, INPUT_CONVERTORS
 from valenspy.input import InputManager
 #Processing
-from valenspy.processing import remap_cdo, select_region, select_point, get_shapefile_mask
+from valenspy.processing import *
 #Diagnostic
 from valenspy.diagnostic import Diagnostic, Model2Ref, Ensemble2Ref, Ensemble2Self
 from valenspy.diagnostic.visualizations import *
