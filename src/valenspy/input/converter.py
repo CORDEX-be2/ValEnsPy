b971d3fa41c7d92c6ac7bd16afcfdae6e8d501ed
Kobe Vandelanotte <61313519+kobebryant432@users.noreply.github.com>
2025-03-21 08:47:48 +0100

    Simplyfing the input convertors (#220)
    * Simplyfing the input convertors
    
    * bug fixes and further clean up
    
    * Shared IC functionality in the IC class
    
    * simple visual notebook IC tests on hortense
    
    * update tests
    
    * Fix for CCLM time freq issue

--- src/valenspy/input/converter.py
+++ src/valenspy/input/converter.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 from typing import Callable, Union
 import xarray as xr
-from valenspy._utilities import load_xarray_from_data_sources
+from valenspy._utilities import load_xarray_from_data_sources, load_yml, cf_status
+from valenspy.input.unit_converter import convert_all_units_to_CF
 from valenspy.input.converter_functions import (
     EOBS_to_CF,
     ERA5_to_CF,
-    ERA5Land_to_CF,
-    CLIMATE_GRID_to_CF,
     CCLM_to_CF,
     ALARO_K_to_CF,
     RADCLIM_to_CF,
@@ -16,16 +15,25 @@ from valenspy.input.converter_functions import (
 class InputConverter:
     """A class that converts input files to netCDF files in CF convention."""
 
-    def __init__(self, converter: Callable):
+    def __init__(self, lookup, converter: Callable = None, metadata_info: dict = None):
         """Initialize the InputProcessor with a converter function.
 
         Parameters
         ----------
-        converter : function
-            The function to convert the input file(s) to a netCDF file in CF convention.
-            The function should take a string or list of strings as input and return a netCDF file.
+        lookup : str | dict | file
+            The lookup table to convert variables (names and units) to CF convention.
+            It can be a string (name of the lookup table in the ancilliary_data folder),
+            It can be a dictionary (keys are the variable names and values are the CF names),
+            It can be a json file (path to the json file containing the lookup table).
+        converter : function, optional
+            A function that deals with unique aspects of the input data when converting to CF convention. Default is None.
+            This function is applied after the units are converted to CF convention and is the last step in the conversion process.
+        metadata_info : dict, optional
+            A dictionary containing the metadata information for the netCDF file.
         """
         self.converter = converter
+        self.lookup = lookup if isinstance(lookup, dict) else load_yml(lookup)
+        self.metadata_info = metadata_info
 
     def convert_input(self, data_sources, metadata_info=None):
         """Convert the input file(s)/xarray dataset to CF convention.
@@ -34,6 +42,8 @@ class InputConverter:
         ----------
         data_sources : Path or list(Path) or xarray.Dataset
             The input file or list of input files or an xarray dataset to convert.
+        metadata_info : dict, optional
+            A dictionary containing additional metadata information for the netCDF file.
 
         Returns
         -------
@@ -41,22 +51,30 @@ class InputConverter:
             An xarray dataset in CF convention.
         """
         ds = load_xarray_from_data_sources(data_sources)
-        return self.converter(ds, metadata_info)
 
+        if self.converter:
+            ds = self.converter(ds)
+        
+        metadata_info = {**self.metadata_info, **metadata_info}
+
+        ds = convert_all_units_to_CF(ds, self.lookup, metadata_info)
+        ds = _set_global_attributes(ds, metadata_info)
+        
+        cf_status(ds)
+
+        return ds
+
+def _set_global_attributes(ds: xr.Dataset, metadata_info):
+    for key, value in metadata_info.items():
+        ds.attrs[key] = value
+    return ds
 
 INPUT_CONVERTORS = {
-    "ERA5": InputConverter(ERA5_to_CF),
-    "ERA5-Land": InputConverter(ERA5Land_to_CF),
-    "EOBS": InputConverter(EOBS_to_CF),
-    "CLIMATE_GRID": InputConverter(CLIMATE_GRID_to_CF),
-    "CCLM": InputConverter(CCLM_to_CF),
-    "ALARO_K": InputConverter(ALARO_K_to_CF),
-    "RADCLIM": InputConverter(RADCLIM_to_CF),
+    "ERA5": InputConverter("ERA5_lookup", ERA5_to_CF, metadata_info={"dataset": "ERA5"}),
+    "ERA5-Land": InputConverter("ERA5_lookup", ERA5_to_CF, metadata_info={"dataset": "ERA5-Land"}),
+    "EOBS": InputConverter("EOBS_lookup", EOBS_to_CF, metadata_info={"freq": "day", "spatial_resolution": "0.1deg", "region": "Europe", "dataset": "EOBS"}),
+    "CLIMATE_GRID": InputConverter("CLIMATE_GRID_lookup", metadata_info={"freq": "day", "spatial_resolution": "0.07° x 0.045° (~5km)", "region": "Belgium", "dataset": "CLIMATE_GRID"}),
+    "CCLM": InputConverter("CCLM_lookup", CCLM_to_CF, metadata_info={"dataset": "CCLM"}),
+    "ALARO_K": InputConverter("ALARO-SFX_K_lookup", ALARO_K_to_CF, metadata_info={"dataset": "ALARO_K"}),
+    "RADCLIM": InputConverter("RADCLIM_lookup", RADCLIM_to_CF, metadata_info={"freq": "hour", "region": "Belgium", "dataset": "RADCLIM"}),
 }
-
-# Idea is to extend the shared functionality here (with subclasses if required) while the inputconvertor_functions are model specific.
-
-# Needed:
-#  - Some helper functions to extend input to glob arguments, str arguments etc.
-#  - Extend input so that already loaded datasets can also be input
-#  - CF Checker functionality
--- src/valenspy/input/converter_functions.py
+++ src/valenspy/input/converter_functions.py
@@ -5,9 +5,7 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 
-
 CORDEX_VARIABLES = load_yml("CORDEX_variables")
-
 
 def _set_global_attributes(ds: xr.Dataset, metadata_info):
     for key, value in metadata_info.items():
@@ -15,8 +13,19 @@ def _set_global_attributes(ds: xr.Dataset, metadata_info):
 
     return ds
 
+def _fix_lat_lon(ds: xr.Dataset):
+    # rename dimensions if not yet renamed
+    if "lon" not in ds.coords:
+        ds = ds.rename({"longitude": "lon"})
+    if "lat" not in ds.coords:
+        ds = ds.rename({"latitude": "lat"})
 
-def EOBS_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
+    # make sure lat and lon are sorted ascending
+    ds = ds.sortby("lat").sortby("lon")
+    return ds
+
+
+def EOBS_to_CF(ds: xr.Dataset) -> xr.Dataset:
     """
     Convert the EOBS xarray netCDF to a CF compliant xarray Dataset
 
@@ -32,34 +41,7 @@ def EOBS_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
     Dataset
         The CF compliant EOBS observations for the specified variable.
     """
-    # Unique information regarding the EOBS dataset
-    obsdata_name = "EOBS"
-    raw_LOOKUP = load_yml(f"{obsdata_name}_lookup")
-
-    if metadata_info is None:  # Set standard metadata if not provided
-        metadata_info = {
-            "freq": "daily",
-            "spatial_resolution": "0.1deg",
-            "region": "europe",
-        }
-
-    # Convert all units to CF, add metadata and set global attributes
-    metadata_info["dataset"] = obsdata_name
-
-    ds = convert_all_units_to_CF(ds, raw_LOOKUP, metadata_info)
-    ds = _set_global_attributes(ds, metadata_info)
-
-    # rename dimensions if not yet renamed
-    if "lon" not in ds.coords:
-        ds = ds.rename({"longitude": "lon"})
-    if "lat" not in ds.coords:
-        ds = ds.rename({"latitude": "lat"})
-
-    # make sure lat and lon are sorted ascending
-    ds = ds.sortby("lat").sortby("lon")
-
-    # Soft check for CF compliance
-    cf_status(ds)
+    ds = _fix_lat_lon(ds)
 
     return ds
 
@@ -80,124 +62,17 @@ def ERA5_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
     Dataset
         The CF compliant ERA5 observations for the specified variable.
     """
-    # Unique information regarding the ERA5 dataset
-    obsdata_name = "ERA5"
-    raw_LOOKUP = load_yml(f"{obsdata_name}_lookup")
-
-    if metadata_info is None:  # Set standard metadata if not provided
-        metadata_info = {"freq": _determine_time_interval(ds)}
-
-    # Convert all units to CF, add metadata and set global attributes
-    metadata_info["dataset"] = obsdata_name
 
     # bugfix ERA5 (found in clh): replace valid_time by time
     if "time" not in ds:
         ds = ds.rename({"valid_time": "time"})
 
-    ds = convert_all_units_to_CF(ds, raw_LOOKUP, metadata_info)
-    ds = _set_global_attributes(ds, metadata_info)
-
-    # rename dimensions if not yet renamed
-    if "lon" not in ds.coords:
-        ds = ds.rename({"longitude": "lon"})
-    if "lat" not in ds.coords:
-        ds = ds.rename({"latitude": "lat"})
-
-    # make sure lat and lon are sorted ascending
-    ds = ds.sortby("lat").sortby("lon")
-
-    cf_status(ds)
+    ds = _fix_lat_lon(ds)
 
     return ds
 
 
-def ERA5Land_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
-    """
-    Convert the ERA5-Land xarray dataset to a xarray Dataset in CF convention.
-    Uses the same lookup table as ERA5.
-
-    Parameters
-    ----------
-    ds : xr.Dataset
-        The xarray Dataset of ERA5-Land observations to convert
-    metadata_info : dict, optional
-        A dictionary containing additional metadata information to add to the dataset
-
-    Returns
-    -------
-    Dataset
-        The CF compliant ERA5-Land observations for the specified variable.
-    """
-    # Unique information regarding the ERA5 dataset
-    obsdata_name = "ERA5-Land"
-    raw_LOOKUP = load_yml(
-        f"ERA5_lookup"
-    )  # Note that the ERA5 lookup is used for both ERA5 and ERA5-Land
-
-    if metadata_info is None:  # Set standard metadata if not provided
-        metadata_info = {"freq": _determine_time_interval(ds)}
-
-    # Convert all units to CF, add metadata and set global attributes
-    metadata_info["dataset"] = obsdata_name
-
-    # bugfix ERA5 (found in clh): replace valid_time by time
-    if "time" not in ds:
-        ds = ds.rename({"valid_time": "time"})
-
-    ds = convert_all_units_to_CF(ds, raw_LOOKUP, metadata_info)
-    ds = _set_global_attributes(ds, metadata_info)
-
-    # rename dimensions if not yet renamed
-    if "lon" not in ds.coords:
-        ds = ds.rename({"longitude": "lon"})
-    if "lat" not in ds.coords:
-        ds = ds.rename({"latitude": "lat"})
-
-    # make sure lat and lon are sorted ascending
-    ds = ds.sortby("lat").sortby("lon")
-
-    cf_status(ds)
-
-    return ds
-
-
-def CLIMATE_GRID_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
-    """
-    Convert the CLIMATE_GRID xarray dataset to a xarray Dataset in CF convention
-
-    Parameters
-    ----------
-    ds : xr.Dataset
-        The xarray Dataset of CLIMATE_GRID observations to convert
-    metadata_info : dict, optional
-        A dictionary containing additional metadata information to add to the dataset
-
-    Returns
-    -------
-    Dataset
-        The CF compliant CLIMATE_GRID observations for the specified variable.
-    """
-    obsdata_name = "CLIMATE_GRID"
-    raw_LOOKUP = load_yml(f"{obsdata_name}_lookup")
-
-    if metadata_info is None:  # Set standard metadata if not provided
-        metadata_info = {
-            "freq": "daily",
-            "spatial_resolution": "0.07° x 0.045° (~5km)",
-            "region": "belgium",
-        }
-
-    metadata_info["dataset"] = obsdata_name
-
-    ds = convert_all_units_to_CF(ds, raw_LOOKUP, metadata_info)
-    ds = _set_global_attributes(ds, metadata_info)
-
-    cf_status(ds)
-
-    return ds
-
-
-def CCLM_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
+def CCLM_to_CF(ds: xr.Dataset) -> xr.Dataset:
     """
     Convert the CCLM xarray netCDF to a CF compliant xarray Dataset
 
@@ -214,39 +89,23 @@ def CCLM_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
         The CF compliant CCLM model data for the specified variable.
     """
 
-    # open observational specific lookyp dictionary - now hardcoded for EOBS, but this can be automated, potentially in the Path generator?
-    model_name = "CCLM"
+    #For each variable in the dataset which has a pressure dimension, create a new variable with the pressure level in the name and remove the pressure dimension
+    if "pressure" in ds.dims:
+        for var in ds.data_vars:
+            if "pressure" in ds[var].dims:
+                for pressure in ds[var].pressure.values:
+                    new_var = var + str(int(pressure / 100)) + "p"
+                    ds[new_var] = ds[var].sel(pressure=pressure)
+                ds = ds.drop_vars(var)
+        ds = ds.drop_dims("pressure")
 
-    # open observational specific lookup dictionary
-    raw_LOOKUP = load_yml(f"{model_name}_lookup")
-
-    if metadata_info is None:  # Set standard metadata if not provided
-        metadata_info = {"experiment": ""}
-
-    metadata_info["dataset"] = model_name
-
-    # if working with pressure levels, the variable in the dataset is still the original name, and there is a pressure level coordinate
-    # e.g. ta500 is T500p in the file name (raw_var), but still T in the dataset. 
-    # therefore, if the pressure coordinate is available, rename the variable to the pressure level, matching the file name. 
-    if 'pressure' in ds.coords:
-        for raw_var in ds.data_vars:
-            raw_var_pressure = raw_var+str(int(ds.pressure.values[0]/100))+'p'
-            var = next(
-                (k for k, v in raw_LOOKUP.items() if v.get("raw_name") == raw_var_pressure), None
-            )
-            if var: 
-                ds = ds.rename_vars({raw_var: raw_var_pressure})
-    
-    ds = ds.assign_coords(time=ds.time.astype('datetime64[D]') + np.timedelta64(12, 'h'))
-
-    ds = convert_all_units_to_CF(ds, raw_LOOKUP, metadata_info)
-
-    # set attributes in whole dataset
-    ds = _set_global_attributes(ds, metadata_info)
-
-    # Soft check for CF compliance
-    cf_status(ds)
-
+    # Seems to be failing because the last time step of the dataset is at 11:00:00, while all others are at 11:30:00.
+    # One option: 
+    ds = ds.isel(time=slice(0,-1))
+    # Other options:
+    # ds = ds.assign_coords(time=(ds.time.dt.floor('H'))) Note ciel does not work as the last time step is at 11:00:00 and the ceil function will round it to 12:00:00
+    # Original option (only for hourly data)
+    # new_time = ds_cclm.tas.time.astype('datetime64[D]') + np.timedelta64(12, 'h')
     return ds
 
 
@@ -266,16 +125,6 @@ def ALARO_K_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
     Dataset
         The CF compliant CCLM model data for the specified variable.
     """
-    model_name = "ALARO-SFX_K"
-    raw_LOOKUP = load_yml(model_name + "_lookup")
-
-    if metadata_info is None:  # Set standard metadata if not provided
-        metadata_info = {}
-
-    metadata_info["dataset"] = model_name
-
-    ds = convert_all_units_to_CF(ds, raw_LOOKUP, metadata_info)
-
     # # Special conversion for precipitation
     # if "rain_convective" in ds.data_vars and "rain_stratiform" in ds.data_vars:
     #     ds["pr"] = ds["rain_convective"] + ds["rain_stratiform"]
@@ -298,12 +147,7 @@ def ALARO_K_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
     #     ds.coords["year_month"] = ds["time.year"] * 100 + ds["time.month"]
     #     ds["pr"] = ds["pr"].groupby("year_month").apply(decumilate)
 
-    ds = _set_global_attributes(ds, metadata_info)
-
-    cf_status(ds)
-
     return ds
-
 
 def RADCLIM_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
     """
@@ -322,31 +166,11 @@ def RADCLIM_to_CF(ds: xr.Dataset, metadata_info=None) -> xr.Dataset:
         The CF compliant CCLM model data for the specified variable.
     """
 
-    # open observational specific lookyp dictionary - now hardcoded for EOBS, but this can be automated, potentially in the Path generator?
-    obsdata_name = "RADCLIM"
-    raw_LOOKUP = load_yml(f"{obsdata_name}_lookup")
-
-    if metadata_info is None:  # Set standard metadata if not provided
-        metadata_info = {
-            "freq": "hourly",
-            "region": "belgium",
-        }
-
-    if "history" not in metadata_info.keys():
-        metadata_info["history"] = ""
-
-    metadata_info["dataset"] = obsdata_name
-
     ds = ds.set_coords(("lat_bounds", "lon_bounds"))
-
-    ds = convert_all_units_to_CF(ds, raw_LOOKUP, metadata_info)
-    ds = _set_global_attributes(ds, metadata_info)
 
     if "nlon" in ds.dims:
         ds = ds.rename({"nlon": "lon"})
     if "nlat" in ds.dims:
         ds = ds.rename({"nlat": "lat"})
-
-    cf_status(ds)
 
     return ds
