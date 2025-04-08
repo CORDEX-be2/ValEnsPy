3ad599945d4117eb767c0319e648eb4d2f697c7e
kobebryant432 <kobe.ande@gmail.com>
2025-04-08 16:29:40 +0200

    bug fix

--- src/valenspy/processing/__init__.py
+++ src/valenspy/processing/__init__.py
@@ -1,4 +1,4 @@
-from .regrid import remap_cdo, remap_xesmf
+from .regrid import remap_xesmf
 from .select import select_region, select_point, get_shapefile_mask
 from .mask import add_prudence_regions
 from .units import convert_units_to
