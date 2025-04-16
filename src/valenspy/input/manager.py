ba35102baedefe2ead3a50fc14020277d396fc01
kobebryant432 <kobe.ande@gmail.com>
2025-04-16 15:42:17 +0200

    ESM_intake first attempt

--- src/valenspy/input/manager.py
+++ src/valenspy/input/manager.py
@@ -3,9 +3,13 @@ from pathlib import Path
 import pandas as pd
 import xarray as xr
 from datatree import DataTree
+from intake_esm.cat import Assets, Attribute, AggregationControl, ESMCatalogModel
+import intake
 import re
 import os
 import glob
+import warnings
+
 
 from valenspy.input.converter import INPUT_CONVERTORS
 from valenspy._utilities import load_yml, create_named_regex
@@ -14,6 +18,7 @@ DATASET_PATHS = load_yml("dataset_PATHS")
 CORDEX_VARIABLES = load_yml("CORDEX_variables")
 
 #TODO - update documentation once re-implemented including type hints
+#This is an type of ECGtools light version
 
 class InputManager:
     COLS = [
@@ -32,7 +37,7 @@ class InputManager:
     "time_range",
     ]
 
-    def __init__(self, machine, catalog_name="catalog.csv", path_format="cmip6-cordex"):
+    def __init__(self, machine : str, dataset_info : dict = None):
         """
         Initialize the InputManager.
 
@@ -41,20 +46,146 @@ class InputManager:
         machine : str
             The name of the machine (used to identify dataset paths).
         dataset_info : dict
-            A dictionary mapping machine names to dataset root paths.
-        catalog_name : str, optional
-            The name of the catalog file (default is "catalog.csv").
-        path_format : str, optional
-            The format of the file paths for parsing metadata (default is "cmip6-cordex").
+            A dictionary containing dataset information. The keys are dataset names and the values are dictionaries with the following keys:
+            - root: The root directory of the dataset.
+            - pattern: The regex pattern for matching files in the dataset. This is the reletave path starting from the root and in the following format:
+                <indentifier_name>/<indentifier_name>/<indentifier_name>_fixed_part_<variable_id>/<another_identifier>_<year>.nc
+            - meta_data: A dictionary containing metadata for the dataset.
+            Default is None. If None, the built-in dataset_info for the provided machine is used. See the dataset_PATHS.yaml file.
         """
         self.machine = machine
-        self.datasets_yaml = DATASET_PATHS[machine]
-        self.path_format = path_format
+
+        if dataset_info:
+            self.datasets_yaml = dataset_info
+        else:
+            self.datasets_yaml = DATASET_PATHS[machine]
+
+        self._validate_dataset_info()
+
         self.df = self.create_catalog()
 
-    def update_catalog(self, dataset_name, dataset_root_dir, dataset_pattern, metadata={}):
+    @classmethod
+    def from_yaml(cls, yaml_path):
         """
-        Add a new dataset to the catalog.
+        Create an InputManager instance from a ESM-catalog YAML file.
+        """
+        self.cat = intake.open_esm_datastore(yaml_path)
+        self.df = self.cat._df
+
+    @property
+    def readable_catalog(self):
+        """
+        Return a readable version of the catalog DataFrame.
+        """
+        COLS = [
+        #Required for unique identification of the dataset
+        "source_id", #The source_id is the name of the dataset (e.g. "ERA5", "CNRM-CM6-1")
+        "frequency", #The frequency of the data (e.g. "daily", "monthly")
+
+        "variable_id",
+        "time_range",
+        ]
+        cols = [item for item in COLS if item not in ["variable_id", "time_range"]]
+        def count(x):
+            return len(x)
+
+        return self.df.groupby(cols)["variable_id"].apply(count).to_frame()
+
+    @property
+    def available_datasets(self):
+        """
+        Return a list of available datasets in the catalog.
+        """
+        return self.df["source_id"].unique().tolist()
+
+    def create_intake_esm_json_catalog(
+        self, 
+        output_path,
+        path_column_name="path",
+        variable_column_name="variable_id",
+        data_format="netcdf",
+        groupby_attrs=None,
+        aggregations=None,
+        esmcat_version:str = "0.1.0",
+        description:str=None,
+    ):
+        """
+        Create an intake-esm JSON catalog from the datasets in the catalog.
+
+        Inspired on ecgtools save functionality
+        """
+        for col in {variable_column_name, path_column_name}.union(set(groupby_attrs or [])):
+            assert col in self.df.columns, f"Column {col} not found in DataFrame"
+
+        attributes = [Attribute(column_name=column, vocabulary="") for column in self.df.columns]
+
+        _aggregation_control = AggregationControl(
+            variable_column_name=variable_column_name,
+            groupby_attrs=groupby_attrs,
+            aggregations=aggregations,
+        )
+
+        cat = ESMCatalogModel(
+            esmcat_version=esmcat_version,
+            description=description,
+            attributes=attributes,
+            aggregation_control=_aggregation_control,
+            assets=Assets(column_name=path_column_name, format=data_format),
+        )
+
+        cat._df = self.df
+
+        self.cat = cat
+
+    def _validate_dataset_info(self):
+
+        required_identifiers = [
+            "source_id",
+            "activity_id",
+            "domain_id"
+        ]
+
+        required_for_filtering = [
+            "frequency",
+            "period"
+        ]
+
+        for dataset_name, dataset_info in self.datasets_yaml.items():
+            key_set = set(dataset_info.get("meta_data", {}).keys())
+            key_set.update({"source_id"}) 
+            pattern = dataset_info.get("pattern", None)
+            if pattern:
+                # Extract the identifiers from the pattern
+                key_set.update(re.findall(r"<(.*?)>", pattern))
+
+            # Check if all required identifiers are present
+            for identifier in required_identifiers:
+                if identifier not in key_set:
+                    warnings.warn(f"Dataset {dataset_name} is missing the required identifier '{identifier}' in its pattern or metadata.")
+
+            # Check if all required identifiers for filtering are present
+            for identifier in required_for_filtering:
+                if identifier not in key_set:
+                    warnings.warn(f"Dataset {dataset_name} is missing the required identifier '{identifier}' for filtering in its pattern or metadata.")
+
+    def update_catalog_from_yaml(self, yaml_path):
+        """
+        Update the catalog from a YAML file.
+
+        Parameters
+        ----------
+        yaml_path : Path
+            The path to the YAML file containing dataset information.
+        """
+        # Load the YAML file
+        datasets_info = load_yml(yaml_path)
+        for dataset_name, dataset_info in datasets_info.items():
+            # Add the new dataset to the catalog
+            self.update_catalog(dataset_name, dataset_info)
+        
+    def update_catalog_from_dataset_info(self, dataset_name, dataset_root_dir, dataset_pattern, metadata={}):
+        """
+        Update the catalog with a new dataset.
 
         Parameters
         ----------
@@ -67,16 +198,32 @@ class InputManager:
         metadata : dict, optional
             Additional metadata to include in the catalog (default is empty dictionary).
         """
-        self.datasets_yaml[dataset_name] = {
+        dataset_info = {
             "root": dataset_root_dir,
             "pattern": dataset_pattern,
-            "metadata": metadata,
+            "meta_data": metadata,
         }
-        data = self.process_dataset_for_catalog(dataset_name, self.datasets_yaml[dataset_name])
+        self.update_catalog(dataset_name, dataset_info)
+    
+    def update_catalog(self, dataset_name, dataset_info_dict):
+        """
+        Add a new dataset to the catalog.
+
+        Parameters
+        ----------
+        dataset_info_dict : dict
+            A dictionary containing dataset information. The keys are dataset names and the values are dictionaries with the following keys:
+            - root: The root directory of the dataset.
+            - pattern: The regex pattern for matching files in the dataset.
+            - meta_data: A dictionary containing metadata for the dataset.
+        """
+        self.datasets_yaml[dataset_name] = dataset_info_dict
+        self._validate_dataset_info()
+        data = self._process_dataset_for_catalog(dataset_name, self.datasets_yaml[dataset_name])
         df = pd.DataFrame(data)
         self.df = pd.concat([self.df, df], ignore_index=True)
 
-    def process_dataset_for_catalog(self, dataset_name, dataset_info):
+    def _process_dataset_for_catalog(self, dataset_name, dataset_info):
         """
         Process all files in a dataset and extract metadata.
         """
@@ -159,39 +306,13 @@ class InputManager:
         files_with_metadata = []
         for dataset_name, dataset_info in self.datasets_yaml.items():
             # Process the dataset and extract metadata
-            grouped_files_with_metadata = self.process_dataset_for_catalog(dataset_name, dataset_info)
+            grouped_files_with_metadata = self._process_dataset_for_catalog(dataset_name, dataset_info)
             # Add the dataset name to the metadata
             files_with_metadata.extend(grouped_files_with_metadata)
             
         # Create a DataFrame and save it as a CSV
         df = pd.DataFrame(files_with_metadata)
         return df
-
-    @property
-    def readable_catalog(self):
-        """
-        Return a readable version of the catalog DataFrame.
-        """
-        COLS = [
-        #Required for unique identification of the dataset
-        "source_id", #The source_id is the name of the dataset (e.g. "ERA5", "CNRM-CM6-1")
-        "frequency", #The frequency of the data (e.g. "daily", "monthly")
-
-        "variable_id",
-        "time_range",
-        ]
-        cols = [item for item in COLS if item not in ["variable_id", "time_range"]]
-        def count(x):
-            return len(x)
-
-        return self.df.groupby(cols)["variable_id"].apply(count).to_frame()
-
-    @property
-    def available_datasets(self):
-        """
-        Return a list of available datasets in the catalog.
-        """
-        return self.df["source_id"].unique().tolist()
 
     def open_dataset(
         self,
