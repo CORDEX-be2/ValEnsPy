bfe05710cf1a0eb938218381c59cbe03bb0c9374
Kobe Vandelanotte <61313519+kobebryant432@users.noreply.github.com>
2026-02-13 09:51:37 +0100

    Revert "Model selection swiss (#245)"
    This reverts commit 4ec34b9bb6e247f0d8d4071747599f46ae25b484.

--- src/valenspy/diagnostic/_ensemble2ref.py
+++ src/valenspy/diagnostic/_ensemble2ref.py
@@ -2,10 +2,7 @@ from valenspy.diagnostic.diagnostic import Ensemble2Ref
 from valenspy.diagnostic.functions import *
 from valenspy.diagnostic.visualizations import *
 
-__all__ = [
-        "MetricsRankings",
-        "EnsembleSubSelection",
-        ]
+__all__ = ["MetricsRankings"]
 
 MetricsRankings = Ensemble2Ref(
     calc_metrics_dt,
@@ -13,22 +10,3 @@ MetricsRankings = Ensemble2Ref(
     "Metrics Rankings",
     "The rankings of ensemble members with respect to several metrics when compared to the reference."
 )
-
-EnsembleSubSelection = Ensemble2Ref(
-    case_sub_selection,
-    {"default":
-        default_plot_kwargs({
-        "x": "var",
-        "y": "abs_change",
-        "selected": ["highest", "middle", "lowest"],
-        "sel_colors": {"highest": "red", "middle": "blue", "lowest": "green"}
-            })(ensemble_selection_boxplot),
-    "heatmap":
-        default_plot_kwargs({
-        "index": "label",
-        "columns": "var",
-        "values": "rel_change"
-        })(ensemble_change_signal_heatmap)},
-    "Ensemble Sub Selection",
-    "The sub selection of ensemble members."
-    )
