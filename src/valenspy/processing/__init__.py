78933f3bac6315ccbf855fd6dc1d21d77b1ef2e2
Kobe Vandelanotte <kobe.vandelanotte@meteo.be>
2026-04-14 16:25:27 +0200

    bug fix import in masking

--- src/valenspy/processing/__init__.py
+++ src/valenspy/processing/__init__.py
@@ -1,5 +1,5 @@
 from .regrid import remap_xesmf
 from .select import select_region, select_point, get_shapefile_mask
-from .mask import mask_dt, add_prudence_regions, 
+from .mask import mask_dt, add_prudence_regions
 from .units import convert_units_to
 from .indicators import xclim_indicator
