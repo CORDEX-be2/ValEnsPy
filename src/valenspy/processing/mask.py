e9405e8255c7f563c69262bc051fc3067cee7d4a
Kobe Vandelanotte <61313519+kobebryant432@users.noreply.github.com>
2026-04-14 15:24:56 +0200

    Masking functionality (#279)
    masking
    
    Co-authored-by: Kobe Vandelanotte <kobe.vandelanotte@meteo.be>

--- src/valenspy/processing/mask.py
+++ src/valenspy/processing/mask.py
@@ -1,6 +1,24 @@
 import regionmask
 import xarray as xr
 
+def mask_dt(dt : xr.DataTree, mask : xr.Dataset | xr.DataArray) -> xr.DataTree:
+    """
+    Mask a datatree with a given mask. The mask will be applied to all datasets in the datatree.
+
+    Parameters
+    ----------
+    dt : xarray.DataTree
+        DataTree to be masked.
+    mask : xarray.Dataset or xarray.DataArray
+        Mask to be applied to the datatree. The mask should have the same dimensions as the datasets in the datatree.
+
+    Returns
+    -------
+    xarray.DataTree
+        Masked datatree.
+    """
+    return dt.map_over_datasets(lambda ds: ds.where(mask) if ds else ds) #Trick to deal with leaves in the datatree that are not datasets (e.g. groups)
+
 def add_prudence_regions(ds : xr.Dataset) -> xr.Dataset:
     """
     Add PRUDENCE regions to a dataset. Regions will be added as a dimension (3D mask).
